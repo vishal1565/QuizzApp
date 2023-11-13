5207d6427c96865b36525db89ded6188ff5f5bdd
Vishal Vishwakarma <vishal.1565@gmail.com>
2023-11-13 19:36:20 +0530

    Initial Commit

---
+++ src/test/java/com/chilgang/quizzApp/QuizzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilgang.quizzApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
